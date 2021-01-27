3c219a46426de28a7c429d289a2f3d616ce69b0f
Ilyar <ilyar.software@gmail.com>
2021-01-27 14:33:36 +0200

    feat: make bottle tonos-cli

--- Formula/tonos-cli.rb
+++ Formula/tonos-cli.rb
@@ -1,14 +1,14 @@
 class TonosCli < Formula
-  desc "Command line tool for TON OS"
+  desc "Command-line tool for TON OS"
   homepage "https://github.com/tonlabs/tonos-cli"
-  license "Apache-2.0"
   url "https://github.com/tonlabs/tonos-cli.git",
-    :tag => "v0.2.0",
-    :using => :git
-  head "https://github.com/tonlabs/tonos-cli.git",
-    :using => :git
+      tag:      "v0.2.0",
+      revision: "5eed060d15e4faf2da2bc4ec6f73b3d32007b4a8"
+  license "Apache-2.0"
+  head "https://github.com/tonlabs/tonos-cli.git"
 
   depends_on "rust" => :build
+  depends_on "openssl"
 
   def install
     system "cargo", "install", *std_cargo_args
@@ -16,6 +16,6 @@ class TonosCli < Formula
 
   test do
     output = shell_output("#{bin}/tonos-cli --version")
-    assert_match "#{version}", output
+    assert_match @version.to_s, output
   end
 end
