11c40db4d8a67adccaccc7a0d30f675deec0605e
Ilyar <761285+ilyar@users.noreply.github.com>
2022-06-03 14:10:07 +0200

    update tvm-solc

--- Formula/tvm-solc.rb
+++ Formula/tvm-solc.rb
@@ -2,17 +2,10 @@ class TvmSolc < Formula
   desc "Solidity compiler for Free TON Virtual Machine"
   homepage "https://github.com/tonlabs/TON-Solidity-Compiler"
   url "https://github.com/tonlabs/TON-Solidity-Compiler.git",
-      tag:      "0.25",
-      revision: "f6cd9bb83439a0e4987155f18d52afe55de1275a"
+      tag:      "0.60.0",
+      revision: "c77554200697489e6a24ab6c863c635a7b4e67ed"
   license "Apache-2.0"
   head "https://github.com/tonlabs/TON-Solidity-Compiler.git"
-
-  bottle do
-    root_url "https://github.com/EverscaleGuild/homebrew-stable/releases/download/tvm-solc-0.25"
-    cellar :any_skip_relocation
-    sha256 "53636577c99f1caf4e71630049c7e691086b6d06170509d6641659e18fccbfee" => :catalina
-    sha256 "cbc242d25a30f927fb5b3beb74dc26b75de44d135e4aae0eeb31acad7852fcbf" => :x86_64_linux
-  end
 
   depends_on "boost" => :build
   depends_on "cmake" => :build
